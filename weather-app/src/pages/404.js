74cae024de8049ee037257b3658bb9c8583bb721
Jorge Berroa <jorgeb174@gmail.com>
2019-04-12 12:06:03 -0400

    updated button file path

--- weather-app/src/pages/404.js
+++ weather-app/src/pages/404.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from "../common/Button";
+import Button from "../Common/Button";
 import { Link } from "react-router-dom";
 export default class NotFound extends Component {
   render() {
