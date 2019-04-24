0a7897c9296de115f03cbed8b7d2eaa93ee74326
jorge berroa <jorgeberroa@jorges-MacBook-Pro-2.local>
2019-04-23 20:26:10 -0400

    updated environment variable names

--- weather-app/src/APIKeys.js
+++ weather-app/src/APIKeys.js
@@ -1,4 +1,4 @@
-export const placesAPIKey = process.env.REACT_APP_placesAPIKey;
-export const mapsAPIKey = process.env.REACT_APP_mapsAPIKey;
-export const yahooAPIKey = process.env.REACT_APP_yahooAPIKey;
-export const geocodeAPIKey = process.env.REACT_APP_geocodeAPIKey;
+export const placesAPIKey = process.env.REACT_APP_PLACES__API__KEY;
+export const mapsAPIKey = process.env.REACT_APP_MAPS__API_KEY;
+export const yahooAPIKey = process.env.REACT_APP_WEATHER_API_KEY;
+export const geocodeAPIKey = process.env.REACT_APP_GEOCODE_API_KEY;
